67a45b85cd5d0af09e049aeef124de19b76bb0f2
Sugama <k-sugama@skyarc.co.jp>
2017-01-20 15:19:03 +0900

    bump version

--- lib/WWW/Flatten.pm
+++ lib/WWW/Flatten.pm
@@ -9,7 +9,7 @@ use Mojo::File;
 use Mojo::Log;
 use WWW::Crawler::Mojo::ScraperUtil qw{resolve_href guess_encoding};
 use Encode;
-our $VERSION = '0.08';
+our $VERSION = '0.09';
 
 my $html_type_regex = qr{^(text|application)/(html|xml|xhtml)};
 my $css_type_regex = qr{^text/css$};
